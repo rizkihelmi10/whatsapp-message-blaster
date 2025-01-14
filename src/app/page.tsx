76e219396fcc0a2b7c58742216e60c36ba0015cd
Rizki Helmi <77191093+rizkihelmi10@users.noreply.github.com>
2025-01-14 17:07:41 +0800

    update page,tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState} from "react";
 
 export default function Home() {
   const [recipients, setRecipients] = useState([{ name: "", phone: "" }]);
